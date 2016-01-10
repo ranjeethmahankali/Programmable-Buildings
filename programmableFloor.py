e6eac3494b351916e8a5e4698df5d020b9347ac0
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2016-01-10 09:26:39 +0530

    Just discovered the 'System' module built into Rhino which is capable of reading colors of individual pixels and other data related to the image

--- programmableFloor.py
+++ programmableFloor.py
@@ -1,8 +1,14 @@
 import rhinoscriptsyntax as rs
 import math
 import time
+import System
 #created on 9th Jan 2015
 
 filePath = rs.OpenFileName()
-view = rs.CurrentView()
-rs.Wallpaper(view, filePath)
+bitmap = System.Drawing.Bitmap.FromFile(filePath)
+
+width = bitmap.Width
+height = bitmap.Height
+color = System.Drawing.Bitmap.GetPixel(bitmap, width/2, height/2)
+print color
+rvalue = color.R
