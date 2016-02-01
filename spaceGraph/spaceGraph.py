f8841c0a5ef64fbdd7a107b29c85b06eb86347db
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2016-02-01 21:32:05 +0530

    Just creating a safe commit before modifying the pathfinder functions to account for parents and children

--- spaceGraph/spaceGraph.py
+++ spaceGraph/spaceGraph.py
@@ -198,28 +198,3 @@ class cSpace:
             err = 'cannot find a path between non-siblings'
             reportError(err)
             return None
-
-#This code is for testing the above classes and defnitions
-
-sg = cSpace('spaceGraph')
-
-sg.addChildren([cSpace('A'), cSpace('B'), cSpace('C'), cSpace('D')])
-sg.addChildren([cSpace('E'), cSpace('F')])
-sg.c['A'].addChildren([cSpace('A1'),cSpace('A2'),cSpace('A3')])
-sg.c['A'].connectChildren('A1','A3')
-sg.c['A'].c['A2'].addChildren([cSpace('A21'),cSpace('A22'),cSpace('A23')])
-
-sg.connectChildren('A', 'B')
-sg.connectChildren('B', 'C')
-sg.connectChildren('B', 'D')
-sg.connectChildren('C', 'D')
-sg.connectChildren('E', 'F')
-sg.connectChildren('F', 'C')
-
-A = sg.c['A']
-path = A.findAllPathsTo('F')
-#print(path)
-short = A.findShortestPathTo('F')
-#print(short)
-A22 = sg.c['A'].c['A2'].c['A23']
-print(A22.hasAncestor('A1'))
