32a708502ecf3ed699466f4a37ef537364fdf618
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2016-03-14 04:48:38 +0530

    now the command line also takes parameters along with the command, separated by a space

--- spaceGraph/spaceConsole.py
+++ spaceGraph/spaceConsole.py
@@ -11,7 +11,14 @@ def createSpace():
 def printRoots():
     print(sg.rootLabels())
 
+def show(*arr):
+    i = 0
+    while i <  len(arr):
+        print(arr[i], len(arr[i]))
+        i += 1
+
 command = {
+            'show' : show,
             'quit' : quit,
             'space' : createSpace,
             'roots' : printRoots
@@ -19,8 +26,45 @@ command = {
 
 while True:
     commandLine = input('>>> ')
+    param = []
+
+    i = 0
+    j = 0
+    num = 0
+    while i < len(commandLine):
+        if commandLine[i] == ' ':
+            if i != 0:
+                if j == 0:
+                    subStr = commandLine[:i]
+                else:
+                    subStr = commandLine[j+1:i]
+                if len(subStr) > 0:
+                    if num == 0:
+                        cmd = subStr
+                    else:
+                        param.append(subStr)
+                    num += 1
+            j = i
+
+        if i == len(commandLine)-1:
+            if j == 0:
+                subStr = commandLine[j:]
+            else:
+                subStr = commandLine[j+1:]
+            
+            if len(subStr) > 0:
+                if num == 0:
+                    cmd = subStr
+                else:
+                    param.append(subStr)
+                num += 1
+        
+        i += 1
+            
     #os.system('cls')
-    if commandLine in command:
-        command[commandLine]()
+    #print(cmd, len(cmd))
+    #print(param)
+    if cmd in command:
+        command[cmd](*param)
     else:
         print('unkown command')
